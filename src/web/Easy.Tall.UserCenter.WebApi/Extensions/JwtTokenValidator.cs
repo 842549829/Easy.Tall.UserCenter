c4024b142857324f04a05359fa89bae38a816627
荣波 <842549829@qq.com>
2019-03-08 13:53:49 +0800

    add

--- src/web/Easy.Tall.UserCenter.WebApi/Extensions/JwtTokenValidator.cs
+++ src/web/Easy.Tall.UserCenter.WebApi/Extensions/JwtTokenValidator.cs
@@ -3,7 +3,6 @@ using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
 using System.Text;
 using Easy.Tall.UserCenter.Framework.Constant;
-using Easy.Tall.UserCenter.Framework.Encrypt;
 using Microsoft.Extensions.Options;
 using Microsoft.IdentityModel.Tokens;
 
