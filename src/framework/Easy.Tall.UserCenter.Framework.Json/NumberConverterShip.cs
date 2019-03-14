a8c5d8d267d9832bb9f3718ffe21697f9c8fc6f6
荣波 <842549829@qq.com>
2019-03-14 15:13:46 +0800

    更新json序列化方式

---
+++ src/framework/Easy.Tall.UserCenter.Framework.Json/NumberConverterShip.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Easy.Tall.UserCenter.Framework.Json
+{
+    /// <summary>
+    /// 转换成字符串的类型
+    /// </summary>
+    [Flags]
+    public enum NumberConverterShip
+    {
+        /// <summary>
+        /// 长整数
+        /// </summary>
+        Int64 = 1,
+
+        /// <summary>
+        /// 无符号长整数
+        /// </summary>
+        UInt64 = 2,
+
+        /// <summary>
+        /// 浮点数
+        /// </summary>
+        Single = 4,
+
+        /// <summary>
+        /// 双精度浮点数
+        /// </summary>
+        Double = 8,
+
+        /// <summary>
+        /// 大数字
+        /// </summary>
+        Decimal = 16
+    }
+}
